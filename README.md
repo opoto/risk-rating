4440b961e128161efd0bbc35c76c1963ad2f8fca
opoto <olivier.potonniee@gmail.com>
2016-09-07 23:27:24 +0200

    Added credits

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # risk-rating
+
 Risk rating web app based on OWASP methodology
+
+Use it here: https://opoto.github.io/risk-rating
--- js/risk-rating.js
+++ js/risk-rating.js
@@ -107,9 +107,11 @@ for (var i=0; i < level2.length; i++) {
 }
 
 // ------------- Google analytics
+var x = window.location.toString().indexOf("://opoto.github.io/risk-rating");
+var gaid = ((x==4) || (x==5)) ?  'UA-83851302-1' : '';
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-83851302-1', 'auto');
+ga('create', gaid, 'auto');
 ga('send', 'pageview');
